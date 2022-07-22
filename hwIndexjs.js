7fbb3cf873f15bb6cbc7651fb9595efd9100bec7
designbyhye <seo421717@gmail.com>
2022-07-22 10:01:03 +0900

    인덱스수정

--- hwIndexjs.js
+++ hwIndexjs.js
@@ -19,13 +19,13 @@ slider.on('wheel', (function(e) {
 
 
 //모달팝업
-$(".projA").click(function(){
+$("#pop1").click(function(){
   $(".popupA").show();
 })
-$(".projB").click(function(){
+$("#pop2").click(function(){
   $(".popupB").show();
 })
-$(".projC").click(function(){
+$("#pop3").click(function(){
   $(".popupC").show();
 })
 
@@ -36,70 +36,67 @@ $(document).mouseup(function (e){
   }
 });
 
-
-//메인텍스트효과
-const textOne = document.querySelector('.text--1');
-const textTwo = document.querySelector('.text--2');
-const logo = document.querySelector('.logo');
-const wrap = document.querySelector('.text__wrap');
-
-const intro = () => {
-  const scaleTL = new TimelineMax();
-  scaleTL.
-  from(logo, 0.5, { scale: 0, opacity: 0, display: 'block' }).
-  to(textOne, 0.25, { text: '디자이너로' }, 0.5).
-  to(wrap, 0.125, { scale: 1.25, delay: 0.125 }).
-  to(wrap, 0.125, { scale: 1 }).
-  to(logo, 0, { opacity: 0, display: 'none' }, 1.5);
-  return scaleTL;
-};
-const slidingWords = () => {
-  const swipeTL = new TimelineMax();
-  swipeTL.
-  to(textOne, 0.15, { delay: 0.1, scale: 0.85 }).
-  to(textOne, 0, { text: '지낸 십여년', delimiter: ' ' }).
-  to(textOne, 0.1, { scale: 1 }).
-  to(wrap, 0.1, { delay: 0.25, scale: 0.75 }).
-  to(wrap, 0.1, { scale: 1 });
-  return swipeTL;
-};
-const turn = () => {
-  const turnTL = new TimelineMax();
-  turnTL.
-  to(wrap, 0, { delay: 0.5, scale: 1.5 }, 0).
-  to(textOne, 0, { delay: 0.5, text: '새로운 전환점' }, 0).
-  to(textTwo, 0, { delay: 0.5, text: '' }, 0).
-  to(wrap, 0, { scale: 1 }, 1);
-  return turnTL;
-};
-const type = (el, text, delay = 0.5) => {
-  const typeTL = new TimelineMax();
-  typeTL.add(TweenMax.to(el, 0, { delay, text: '' }));
-  for (let l = 0; l < text.length; l++) {
-    typeTL.add(TweenMax.to(el, 0.15, { text: text.slice(0, l + 1) }));
-  }
-  return typeTL;
-};
-const startFirst = () => {
-  const startFirstTL = new TimelineMax();
-  startFirstTL.
-  to(textOne, 0, { text: '첫번째', delay: 0.5 }, 0).
-  to(textTwo, 0, { text: '&nbsp;포트폴리오', delay: 0.5 }, 0).
-  from(textOne, 0.25, { x: '-25%', opacity: 0, immediateRender: false }, 0.7).
-  from(textTwo, 0.25, { x: '-125%', opacity: 0, immediateRender: false }, 0.7).
-
-  to(textTwo, 0.25, { x: '0%', text: '' }, 1.25);
-  return startFirstTL;
-};
-const advertTL = new TimelineMax({
-  onComplete: () => {
-    advertTL.restart();
-  }
+//탭메뉴 가로형
+$(".pop > ul > li").click(function(){
+  $(this).addClass("on").siblings().removeClass("on");
+  $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
 });
-advertTL.
-add(intro()).
-add(slidingWords()).
-add(turn()).
-add(type(textOne, '그 시작의')).
-add(startFirst());
+
+
+//메인텍스트
+$('.txt').html(function(i, html) {
+  var chars = $.trim(html).split("");
+
+  return '<span>' + chars.join('</span><span>') + '</span>';
+});
+
+
+
+//배경
+function init(){
+
+  //estrelas
+
+  var style = ["style1", "style2", "style3", "style4"];
+  var tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
+  var opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
+
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  var estrela = "";
+  var qtdeEstrelas = 250;
+  var noite = document.querySelector(".constelacao");
+  var widthWindow = window.innerWidth;
+  var heightWindow = window.innerHeight;
+
+  for (var i = 0; i < qtdeEstrelas; i++) {
+    estrela += "<span class='estrela " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
+    + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." +getRandomArbitrary(0, 9)+ "s; left: "
+    + getRandomArbitrary(0, widthWindow) + "px; top: " + getRandomArbitrary(0, heightWindow) + "px;'></span>";
+  }
+
+  noite.innerHTML = estrela;
+
+  //meteoros
+
+  var numeroAleatorio = 5000;
+
+  setTimeout(function(){
+    carregarMeteoro();
+  }, numeroAleatorio);
+
+  function carregarMeteoro(){
+    setTimeout(carregarMeteoro, numeroAleatorio);
+    numeroAleatorio = getRandomArbitrary(5000, 10000);
+    var meteoro = "<div class='meteoro "+ style[getRandomArbitrary(0, 4)] +"'></div>";
+    document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = meteoro;
+    setTimeout(function(){
+      document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = "";
+    }, 1000);
+  }
+}
+
+window.onload = init;
 
